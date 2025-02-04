675ecc4335f3cb7ef3e0198871c4856807794e6a
04mmii <04mmii@naver.com>
2025-02-04 12:01:24 +0900

    .

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://api.themoviedb.org/3",
-    params: {
-        api_key: "ab091c1360c493c6c44e6b36968850fd",
-        language: "ko-KR",
-    },
+  baseURL: "https://api.themoviedb.org/3",
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_TMDB_ACCESS_TOKEN}`,
+    "Content-Type": "application/json;charset=utf-8",
+  },
+  params: {
+    language: "ko-KR",
+  },
 });
 
 export default instance;
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,52 +1,61 @@
-import axios from '../api/axios.js';
-import React, { useEffect, useState } from 'react';
+import axios from "../api/axios.js";
+import React, { useEffect, useState, useCallback } from "react";
 import "./Row.css";
 
-export default function Row({title, id, fetchUrl, isLargeRow}) {
+export default function Row({ title, id, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
 
-    const [movies, setMovies] = useState([]);
+  const fetchMovieData = useCallback(async () => {
+    try {
+      const request = await axios.get(fetchUrl);
+      console.log("request", request);
+      setMovies(request.data.results);
+    } catch (error) {
+      console.error("Error fetching movie data:", error);
+    }
+  }, [fetchUrl]);
 
-    useEffect(() => {
-        fetchMovieData();
-    }, [ ]);
-    
-    const fetchMovieData = async () => {
-        const request = await axios.get(fetchUrl);
-        console.log("request", request);
-        setMovies(request.data.results);
-    };
+  useEffect(() => {
+    fetchMovieData();
+  }, [fetchMovieData]);
 
-    
   return (
     <section className="row">
-        <h2>{title}</h2>
-        <div className="slider">
-            <div className="slider__arrow-left">
-                <span 
-                    className="arrow"
-                    onClick={() => {
-                        document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-                    }}
-                >{"<"}</span>
-            </div>
-            <div id={id} className="row__posters">
-                {movies.map(movie => (
-                    <img 
-                        key={movie.id}
-                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                        src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-                        alt={movie.name}
-                    />
-                ))};
-            </div>
-            <div className="slider__arrow">
-                <span 
-                    className="arrow"
-                    onClick={() => {
-                        document.getElementById(id).scrollLeft += window.innerWidth - 80;}}
-                >{">"}</span>
-            </div>
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {"<"}
+          </span>
         </div>
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`https://image.tmdb.org/t/p/original/${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        <div className="slider__arrow-right">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >
+            {">"}
+          </span>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
